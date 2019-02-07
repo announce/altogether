e9f50dcd9088a4ddf23e5ef06fa0b961680965ef
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2019-02-08 00:27:35 +0900

    Added spec test

--- main_test.go
+++ main_test.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"flag"
-	"github.com/ToQoz/gopwt"
 	"github.com/ToQoz/gopwt/assert"
-	"os"
 	"testing"
 )
-
-func TestMain(m *testing.M) {
-	flag.Parse()
-	gopwt.Empower()
-	os.Exit(m.Run())
-}
 
 func TestCreateApp(t *testing.T) {
 	app := CreateApp()
