0d9a80b85709baeb2d4592d230f53120ed517afd
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2019-02-09 00:44:11 +0900

    Added verbose log

--- al2/features/web.go
+++ al2/features/web.go
@@ -19,6 +19,10 @@ const (
 	Albert
 )
 
+func (t Type) String() string {
+	return [...]string{"Alfred", "Albert"}[t]
+}
+
 var ConfigPath = map[Type]string{
 	Alfred: "preferences/features/websearch/prefs.plist",
 	Albert: "org.albert.extension.websearch/engines.json",
@@ -57,19 +61,26 @@ type Web struct {
 }
 
 type Option struct {
-	DtyRun bool
+	DtyRun  bool
+	Verbose bool
 }
 
 func (w *Web) Sync(option Option) error {
 	w.init()
+	if err := w.load(); err != nil {
+		return err
+	}
 	direction, err := w.compare()
 	if err != nil {
 		w.log.Println("[Error] Failed to compare mtime", err)
 		return err
-	}
-	if direction == nil {
+	} else if direction == nil {
 		w.log.Println("Aborting... Both target has the same mtime.")
 		return nil
+	}
+	if option.Verbose {
+		w.log.Printf("Direction: (0,1)=(%v, %v)",
+			direction[0].Type, direction[1].Type)
 	}
 	if err := w.parse(direction[0]); err != nil {
 		w.log.Println("[Error] Failed to parse 0-config:", direction[0], err)
@@ -79,7 +90,10 @@ func (w *Web) Sync(option Option) error {
 		w.log.Println("[Error] Failed to parse 1-config:", direction[1], err)
 		return err
 	}
-	w.log.Printf("w.AlfredPrefs: %+v,\nw.AlbertEngines: %+v", w.AlfredPrefs, w.AlbertEngines)
+	if option.Verbose {
+		w.log.Printf("AlfredPrefs: %+v\nAlbertEngines: %+v",
+			w.AlfredPrefs, w.AlbertEngines)
+	}
 
 	// @TODO os.Chtimes to avoid loop
 	return nil
@@ -94,14 +108,18 @@ func (w *Web) init() {
 	w.AlbertEngines = &AlbertEngines{}
 }
 
-func (w *Web) compare() (*Pair, error) {
+func (w *Web) load() error {
 	for _, launcher := range w.Launchers {
 		info, err := os.Stat(launcher.ConfigPath)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		launcher.FileInfo = info
 	}
+	return nil
+}
+
+func (w *Web) compare() (*Pair, error) {
 	if w.Launchers[0].Mtime() == w.Launchers[1].Mtime() {
 		return nil, nil
 	}
