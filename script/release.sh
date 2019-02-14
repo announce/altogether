bb3e18a666a3290b05b78f74f000ddfa90495c81
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2019-02-14 21:51:02 +0900

    Updated release script

--- script/release.sh
+++ script/release.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 XC_ARCH=${XC_ARCH:-amd64}
-XC_OS=${XC_OS:-linux darwin windows}
+XC_OS=${XC_OS:-linux darwin}
 VERSION=${VERSION:-v0.0.0}
 BUILD_FLAGS="-X \"main.Version=${VERSION}\""
 ASSET_DIR="${ASSET_DIR:=pkg}"
+echo "ASSET_DIR:${ASSET_DIR}"
 
-rm -rf pkg/*
+#rm -rf pkg/*
 gox \
     -ldflags="${BUILD_FLAGS}" \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
     -output "${ASSET_DIR}/{{.OS}}-{{.Arch}}/{{.Dir}}"
+
+type zip
+find "${ASSET_DIR}" -mindepth 1 -type d -print0 | xargs -0 -I@ zip -r "@.zip" "@"
