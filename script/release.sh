9701a6b4b33608c53d2e456ba35a6acfdfde3983
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2019-02-14 21:51:02 +0900

    Updated release script

--- script/release.sh
+++ script/release.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 XC_ARCH=${XC_ARCH:-amd64}
-XC_OS=${XC_OS:-linux darwin windows}
+XC_OS=${XC_OS:-linux darwin}
 VERSION=${VERSION:-v0.0.0}
 BUILD_FLAGS="-X \"main.Version=${VERSION}\""
 ASSET_DIR="${ASSET_DIR:=pkg}"
+echo "ASSET_DIR:${ASSET_DIR}"
 
-rm -rf pkg/*
+#rm -rf pkg/*
 gox \
     -ldflags="${BUILD_FLAGS}" \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
     -output "${ASSET_DIR}/{{.OS}}-{{.Arch}}/{{.Dir}}"
+
+ls -la "${ASSET_DIR}/*"
