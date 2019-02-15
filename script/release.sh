717acf27cb36b79dec6d4e5e006dda70dfee75e2
Kenta Yamamoto <ymkjp@jaist.ac.jp>
2019-02-16 01:28:35 +0900

    #7 binary distribution

--- script/release.sh
+++ script/release.sh
@@ -7,12 +7,12 @@ BUILD_FLAGS="-X \"main.Version=${VERSION}\""
 ASSET_DIR="${ASSET_DIR:=pkg}"
 echo "ASSET_DIR:${ASSET_DIR}"
 
-#rm -rf pkg/*
+rm -vrf "${ASSET_DIR:?}/"*
+
 gox \
     -ldflags="${BUILD_FLAGS}" \
     -os="${XC_OS}" \
     -arch="${XC_ARCH}" \
     -output "${ASSET_DIR}/{{.OS}}-{{.Arch}}/{{.Dir}}"
 
-type zip
 find "${ASSET_DIR}" -mindepth 1 -type d -print0 | xargs -0 -I@ zip -r "@.zip" "@"
